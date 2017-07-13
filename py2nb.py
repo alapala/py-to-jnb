125f8da2d78ee5a51f4e8da2594ab64158f01cd1
Alan Parker Lue <alan.parker.lue@gmail.com>
2017-07-13 10:27:27 -0400

    Use new_notebook()

--- py2nb.py
+++ py2nb.py
@@ -13,6 +13,7 @@ parser.add_argument('source', metavar='SOURCE', help='Source file.')
 args = parser.parse_args()
 source_file = args.source
 
+nb = nbf.v4.new_notebook()
 try:
     with open(source_file) as f:
         code = f.read()
@@ -25,14 +26,11 @@ except FileNotFoundError:
         file=sys.stderr
     )
     exit(1)
-nbookv3 = nbf.v3.read_py(code)
-nbookv4 = nbf.v4.upgrade(nbookv3)
-jsonform = nbf.v4.writes(nbookv4)
+nb.cells.append(nbf.v4.new_code_cell(code))
 
 dest_file = re.sub('\.py$', '.ipynb', source_file)
 if os.path.exists(dest_file):
     s = input("Overwrite existing file `{}' (y/[n])? ".format(dest_file))
     if s.casefold() not in {'y', 'yes'}:
         exit()
-with open(dest_file, 'w') as f:
-    f.write(jsonform)
+nbf.write(nb, dest_file)
